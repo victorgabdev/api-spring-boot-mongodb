1d0ee5fbfaef8cf10664400fb9a937b87fae3492
Victor Gabriel <victo091198@gmail.com>
2022-11-14 17:24:45 -0300

    Project created

---
+++ src/main/java/com/api/mongodb/projectSB/ProjectSbApplication.java
@@ -0,0 +1,13 @@
+package com.api.mongodb.projectSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSbApplication.class, args);
+	}
+
+}
