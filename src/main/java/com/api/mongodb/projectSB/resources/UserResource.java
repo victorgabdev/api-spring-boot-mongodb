048a73aa30cb12a42a4dabd3667dda4755dbf5bd
Victor Gabriel <victo091198@gmail.com>
2022-11-14 17:47:39 -0300

    Domain User and Resource User created

---
+++ src/main/java/com/api/mongodb/projectSB/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.api.mongodb.projectSB.resources;
+
+import com.api.mongodb.projectSB.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Silva", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        return ResponseEntity.ok().body(new ArrayList<>(Arrays.asList(maria, alex)));
+    }
+
+}
