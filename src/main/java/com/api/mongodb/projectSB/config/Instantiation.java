976a7d408059e78226cb698b3f5b664789793556
Victor Gabriel <victo091198@gmail.com>
2022-11-17 18:48:40 -0300

    Added comments to post

--- src/main/java/com/api/mongodb/projectSB/config/Instantiation.java
+++ src/main/java/com/api/mongodb/projectSB/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.api.mongodb.projectSB.config;
 import com.api.mongodb.projectSB.domain.Post;
 import com.api.mongodb.projectSB.domain.User;
 import com.api.mongodb.projectSB.dto.AuthorDTO;
+import com.api.mongodb.projectSB.dto.CommentDTO;
 import com.api.mongodb.projectSB.repositories.PostRepository;
 import com.api.mongodb.projectSB.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Configuration
@@ -39,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Patiu Viagem", "Vou viajar para São Paulo!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa Viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/api/mongodb/projectSB/domain/Post.java
+++ src/main/java/com/api/mongodb/projectSB/domain/Post.java
@@ -1,11 +1,14 @@
 package com.api.mongodb.projectSB.domain;
 
 import com.api.mongodb.projectSB.dto.AuthorDTO;
+import com.api.mongodb.projectSB.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,6 +21,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {}
 
@@ -69,6 +73,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -80,6 +92,5 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hash(getId());
     }
-
 
 }
